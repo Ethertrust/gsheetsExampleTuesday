f106e68cbd574f9f94b26b2cde8cf15846ce02e3
Ether <shvedov_es@surgu.ru>
2025-04-01 19:27:26 +0500

    matplotlib shared axes

---
+++ plotlib2.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.patches import ConnectionPatch
+
+# make figure and assign axis objects
+fig, axs = plt.subplots(4, 3, figsize=(14, 8))
+print(fig)
+print(axs)
+ax0 = axs[0,0]
+ax1 = axs[0,1]
+ax2 = axs[0,2]
+# fig.subplots_adjust(wspace=0)
+
+# pie chart parameters
+overall_ratios = [.27, .56, .17]
+labels = ['Approve', 'Disapprove', 'Undecided']
+explode = [0.1, 0, 0]
+# rotate so that first wedge is split by the x-axis
+angle = -180 * overall_ratios[0]
+wedges, *_ = ax1.pie(overall_ratios, autopct='%.2f%%', startangle=angle,
+                     labels=labels, explode=explode)
+print(wedges)
+# bar chart parameters
+age_ratios = [.33, .54, .07, .06]
+age_labels = ['Under 35', '35-49', '50-65', 'Over 65']
+bottom = 1
+width = .2
+
+# Adding from the top matches the legend.
+print(*zip(age_ratios, age_labels))
+print(reversed([*zip(age_ratios, age_labels)]))
+for j, (height, label) in enumerate(reversed([*zip(age_ratios, age_labels)])):
+    bottom -= height
+    bc = ax2.bar(0, height, width, bottom=bottom, color='C0', label=label,
+                 alpha=0.1 + 0.25 * j)
+    ax2.bar_label(bc, labels=[f"{height:.0%}"], label_type='center')
+
+ax2.set_title('Age of approvers')
+ax2.legend()
+ax2.axis('off')
+ax2.set_xlim(- 2.5 * width, 2.5 * width)
+
+# use ConnectionPatch to draw lines between the two plots
+theta1, theta2 = wedges[0].theta1, wedges[0].theta2
+center, r = wedges[0].center, wedges[0].r
+print(center, r)
+bar_height = sum(age_ratios)
+
+# draw top connecting line
+x = r * np.cos(np.pi / 180 * theta2) + center[0]
+y = r * np.sin(np.pi / 180 * theta2) + center[1]
+con = ConnectionPatch(xyA=(-width / 2, bar_height), coordsA=ax2.transData,
+                      xyB=(x, y), coordsB=ax1.transData)
+con.set_color([0, 0, 0])
+con.set_linewidth(4)
+ax1.add_artist(con)
+
+# draw bottom connecting line
+x = r * np.cos(np.pi / 180 * theta1) + center[0]
+y = r * np.sin(np.pi / 180 * theta1) + center[1]
+con = ConnectionPatch(xyA=(-width / 2, 0), coordsA=ax2.transData,
+                      xyB=(x, y), coordsB=ax1.transData)
+con.set_color([0, 0, 0])
+ax2.add_artist(con)
+con.set_linewidth(4)
+
+# Data for plotting
+t = np.arange(0.0, 2.0, 0.01)
+print(t)
+s = 1 + np.sin(2 * np.pi * t)
+print(s)
+ax0.plot(t, s)
+
+ax0.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='About as simple as it gets, folks')
+ax0.grid()
+
+fig.savefig("test.png")
+
+plt.show()
